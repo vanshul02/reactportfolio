24a398611cab9abd73ef75e85d8e1c8b8ae9acb2
Vanshul <vanshul.ahuja@gmail.com>
2022-01-12 16:12:38 +0530

    Changed Ceritficate and Icon

--- src/PortfolioPage/components/AboutTabs.js
+++ src/PortfolioPage/components/AboutTabs.js
@@ -25,8 +25,8 @@ const AboutTabs = () => {
                 <div className="ed-cert">
                     <div className="ed-cert-title">Bachelors in Technology -<span> PDPM Indian Institute of Information Technology (IIIT) Jabalpur</span></div>
                     <p>Nov 2020 - July 2024</p>
-                    <div className="ed-cert-title">Certificate of Completion of Data Structures in C++ -<span> Coding Ninjas</span></div>
-                    <p>Dec 2021</p>
+                    <div className="ed-cert-title">Certificate of Excellence for Data Structures in C++  -<span> Coding Ninjas</span></div>
+                    <p>Dec 2021 &emsp; <a href="http://files.codingninjas.in/certificate17731074a5f8b0812e35d7b9d655b24e612f458.pdf"> Certificate Link </a> </p>
                 </div>
             </TabPanel>
         </Tabs>
